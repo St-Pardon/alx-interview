028944927be420c31f89152633191b4ab674f17a
St Pardon <okc4pardon@gmail.com>
2023-02-10 20:46:40 +0100

    :hammer: rewrote for shorter time complexity

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,15 +8,15 @@ def minOperations(n):
     '''
     if not isinstance(n, int) or n < 0 or n == 1:
         return 0
-    var = 'H'
+    var = 1
     count = 0
-    dup = ''
-    while len(var) < n:
-        if n % len(var) != 0:
+    dup = 0
+    while var < n:
+        if n % var != 0:
             var += dup
             count += 1
         else:
             dup = var
             var += dup
             count += 2
-    return (count if len(var) == n else 0)
+    return (count if var == n else 0)
