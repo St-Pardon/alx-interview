8400596404c0c3dfcc6c60e2cf3c168b25ddca20
St Pardon <okc4pardon@gmail.com>
2023-03-08 17:36:23 +0100

    :white_check_mark: completed challenge

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,18 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+const request = require('request');
+if (process.argv.length > 2) {
+  request(
+    `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+    (err, res, body) => {
+      if (err) console.log(err);
+      const json = JSON.parse(body);
+      json.characters.map((item) => {
+        request(item, (err, res, body) => {
+          if (err) console.log(err);
+          const result = JSON.parse(body);
+          console.log(result.name);
+        });
+      });
+    }
+  );
+}
