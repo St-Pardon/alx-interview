7943569638fa778c262ca136b83164d23573289d
St Pardon <okc4pardon@gmail.com>
2023-03-29 16:37:50 +0100

    :beetle: fixed doc bugs

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,26 +2,19 @@
 '''island perimeter
 '''
 
-from collections import deque
-
 
 def numofIsland(grid, i, j):
     '''Num of islands
     '''
     count = 0
-
     if (i > 0 and grid[i - 1][j]):
         count += 1
-
     if (j > 0 and grid[i][j - 1]):
         count += 1
-
     if (i < len(grid)-1 and grid[i + 1][j]):
         count += 1
-
     if (j < len(grid[0])-1 and grid[i][j + 1]):
         count += 1
-
     return count
 
 
@@ -31,10 +24,8 @@ def island_perimeter(grid):
     C = len(grid[0])
     R = len(grid)
     perimeter = 0
-
     for i in range(0, R):
         for j in range(0, C):
             if (grid[i][j]):
                 perimeter += (4 - numofIsland(grid, i, j))
-
     return perimeter
